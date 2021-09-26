30b477c8d7e7ec4072d07d46f458e509f6c8294a
Cuong Phan <vcuong0909@gmail.com>
2021-09-26 14:56:35 -0400

    completed feature reset password

--- app/src/main/java/com/example/propertymanagement/view/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/propertymanagement/view/ResetPasswordActivity.kt
@@ -1,12 +1,71 @@
 package com.example.propertymanagement.view
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.propertymanagement.R
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.example.propertymanagement.databinding.ActivityResetPasswordBinding
 
 class ResetPasswordActivity : AppCompatActivity() {
+    lateinit var binding: ActivityResetPasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reset_password)
+        binding = ActivityResetPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        modifyTextView()
+        initEvents()
+    }
+
+    private fun initEvents() {
+        binding.btnSendResetLink.setOnClickListener{
+            AlertDialog.Builder(this).apply{
+                setMessage("API not supported")
+                setPositiveButton("Sure") {
+                    dialog, _ -> dialog.dismiss()
+                }
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun modifyTextView() {
+        val text = binding.tvRegister.text.toString()
+        val spannable = SpannableString(text)
+        val clickable = object: ClickableSpan() {
+            override fun onClick(p0: View) {
+                startActivity(Intent(baseContext, RegisterActivity::class.java))
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+                ds.color = Color.DKGRAY
+            }
+        }
+
+        spannable.setSpan(
+            clickable,
+            text.length - "Sign up".length, text.length,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+
+        binding.tvRegister.text = spannable
+        binding.tvRegister.highlightColor = Color.TRANSPARENT
+        binding.tvRegister.movementMethod = LinkMovementMethod.getInstance()
     }
 }
