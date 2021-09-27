03a4d7f8ea2f5f920597e014155129b310a983bd
Cuong Phan <vcuong0909@gmail.com>
2021-09-26 22:00:17 -0400

    need to check if the current user exists already logged in or not

--- app/src/main/java/com/example/propertymanagement/view/MainActivity.kt
+++ app/src/main/java/com/example/propertymanagement/view/MainActivity.kt
@@ -10,40 +10,29 @@ import com.example.propertymanagement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-    val LAUNCH_LOGIN_SCREEN = 100
-    val LAUNCH_HOME_SCREEN = 200
+    companion object {
+        const val LAUNCH_LOGIN_SCREEN = 100
+        const val LAUNCH_DASHBOARD_SCREEN = 200
+    }
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        getwaitingTime()
+        // check the sharedPref file if the user has logged in
+        handler.sendEmptyMessageDelayed(LAUNCH_LOGIN_SCREEN, 3000)
     }
 
-    private fun getwaitingTime() {
-        val screentimeout = 3000L
-
-        handler.sendEmptyMessageDelayed(LAUNCH_HOME_SCREEN, screentimeout)
-
-//        if(LAUNCH_HOME_SCREEN != 0){
-//            handler.sendEmptyMessageDelayed(LAUNCH_HOME_SCREEN, screentimeout)
-//
-//        }else if(LAUNCH_LOGIN_SCREEN != 0){
-//            handler.sendEmptyMessageDelayed(LAUNCH_LOGIN_SCREEN, screentimeout)
-//        }
-
-    }
-
-    val handler = object : Handler(){
-        override fun handleMessage(msg: Message){
+    private val handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
 
-            if (msg.what == LAUNCH_HOME_SCREEN){
+            if (msg.what == LAUNCH_DASHBOARD_SCREEN) {
                 startActivity(Intent(baseContext, DashboardActivity::class.java))
                 finish()
-            }else if(msg.what == LAUNCH_LOGIN_SCREEN){
+            } else if (msg.what == LAUNCH_LOGIN_SCREEN) {
                 startActivity(Intent(baseContext, LoginActivity::class.java))
                 finish()
             }
